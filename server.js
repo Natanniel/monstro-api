be239dd401337d780dd409816a23a4764edbd4a5
Natanniel <natanniel.alves@outlook.com>
2022-05-30 13:00:59 -0300

    deploy

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 var fs = require('fs');
 var mqttServer = require('mqtt-server');
  
-mqttServer({
-  mqtt: 'tcp://localhost:1883',
+servers = mqttServer({
+  mqtt: 'tcp://localhost:61613',
   mqtts: 'ssl://localhost:8883',
   mqttws: 'ws://localhost:1884',
   mqtwss: 'wss://localhost:8884'
 }, {
   ssl: {
-    key: fs.readFileSync('./key.key'),
-    cert: fs.readFileSync('./crt.crt')
+  //  key: fs.readFileSync('./key.key'),
+   // cert: fs.readFileSync('./crt.crt')
   },
   emitEvents: true // default
 }, function(client){
+  console.log('Algo aqui');
+  console.log(client);
+  
   client.connack({
     returnCode: 0
   });
 });
  
 servers.listen(function(){
+  
   console.log('listening!');
+
 });
-
-
-
-
